7f5662982d255dc297988b3068b3ef676a733824
Akmal Rizky Fauzan <106301992+akmalrizk@users.noreply.github.com>
2025-02-13 11:07:20 +0700

    Lesson 1 - Variables

---
+++ main.py
@@ -0,0 +1,53 @@
+# lesson 1 = python variable
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Akmal"
+food = "fried rice"
+email = "akmalrizk2508@gmail.com"
+
+# print(f"Hello {first_name}") # f-string
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+# Integers
+age = 25
+quantity = 3
+num_of_student = 30
+
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+# print(f"Your class has {num_of_student} students")
+
+# Float
+price = 10.99
+gpa = 3.02
+distance = 5.5
+
+# print(f"The price is ${price}")
+# print(f"Your GPA is: {gpa}")
+# print(f"You ran {distance}km")
+
+# Boolean
+
+is_student = False
+for_sale = False
+is_online = False
+
+# print(f"Are you a student?: {is_student}")
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are NOT a student")
+
+# if for_sale:
+#     print("That item is for sale")
+# else:
+#     print("That item is NOT available")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")

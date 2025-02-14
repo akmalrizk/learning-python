525082b23e77edc588e0b84d7c098f371cb6454e
Akmal Rizky Fauzan <106301992+akmalrizk@users.noreply.github.com>
2025-02-14 16:39:06 +0700

    Add user_input_exercise_3.py
    Exercise 3 (Shopping Cart)

---
+++ user_input_exercise_3.py
@@ -0,0 +1,10 @@
+# Exercise 3 (Shopping Cart)
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${round(total, 2)}")

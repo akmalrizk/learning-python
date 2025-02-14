a4f36ae6b026ee5e0f25665e3a5e5f72da2a34b6
Akmal Rizky Fauzan <106301992+akmalrizk@users.noreply.github.com>
2025-02-14 13:40:09 +0700

    Lesson 3 - User Input

--- main.py
+++ main.py
@@ -1,50 +1,8 @@
-# Lesson 2 (Typecasting)
+# Lesson 3 (User Input)
 
-# typecasting = the process of converting a value of one data type to another data type (string to integer, integer to float, float to boolean, etc)
-# Explicit vs Implicit
+name = input("Enter your name: ")
+age = int(input("Enter your age: ")) #typecasting to integer
+age = age + 1
 
-# name = "Akmal Rizky Fauzan"
-# age = 25
-# gpa = 3.02
-# student = False
-
-# print(type(name)) --> to know the data type of the variable
-# print(type(age))
-# print(type(gpa))
-# print(type(student))
-
-# Explicit
-# Integer to Float
-# age = float(age)
-# print(type(age))
-
-# Float to Integer
-# gpa = int(gpa)
-# print(type(gpa))
-
-# Boolean to String
-# student = str(student)
-# print(type(student))
-
-# Integer to Boolean
-# age = bool(age)
-# print(age) 
-# When print the age, it will be True because there are numbers but not ZERO
-# If the number is ZERO, it will be False
-# print(type(age))
-
-# String to Boolean
-# name = bool(name)
-# print(name) 
-# When print the name, it will be True because there are characters but not EMPTY
-# If there an EMPTY string, it will be False
-# print(type(name))
-
-# Implicit
-x = 2
-y = 2.0
-
-x = x/y
-
-print(x)
-# X will become a float because the math operation makes X (integer) turns to like Y (float) data type.
+print(f"Hello {name}!")
+print(f"You are {age} years old")
